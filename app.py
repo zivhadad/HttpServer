2c6a99aa766ade5ce10af836b7f9db92ad3619e0
Ziv Hadad <zivhadad@Zivs-MacBook-Pro-3.local>
2022-07-26 14:36:49 +0300

    check

--- app.py
+++ app.py
@@ -5,14 +5,6 @@ logging.basicConfig(filename='/var/log/webserver.log', encoding='utf-8', level=l
 
 app = flask.Flask(__name__)
 
-port = os.getenv("port")
-if port.isdigit():
-    app.run('0.0.0.0',port)
-    logging.info("Started application")
-else:
-    app.logger.error("Can't start application provided wrong value: ",type(port))
-    logging.error("The application wasn't started because there was wrong value of port ENV")
-
 @app.route("/")
 def _wellcome():
     return flask.send_file('index.html')
@@ -22,3 +14,10 @@ def _healthcheck():
 @app.route("/host_ip")
 def host_ip():
     return flask.request.remote_addr
+port = os.getenv("port")
+if port.isdigit():
+    app.run('0.0.0.0',port)
+    logging.info("Started application")
+else:
+    app.logger.error("Can't start application provided wrong value: ",type(port))
+    logging.error("The application wasn't started because there was wrong value of port ENV")
